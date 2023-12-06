00c1a13d92cd02d3bb837cb3c16141e5ad10529d
Igrar Muradov <igrar.muradov@gmail.com>
2023-12-07 02:03:17 +0400

    adding field validation

---
+++ spring-boot-bootcamp-resources-main/03. field-validation/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/ScoreValidator.java
@@ -0,0 +1,29 @@
+package com.ltp.gradesubmission;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class ScoreValidator implements ConstraintValidator<Score, String> {
+
+    List<String> scores = Arrays.asList(
+            "A+", "A", "A-",
+            "B+", "B", "B-",
+            "C+", "C", "C-",
+            "D+", "D", "D-",
+            "F"
+    );
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        for (String string :
+                scores) {
+            if(value.equals(string)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ spring-boot-bootcamp-resources-main/03. field-validation/workbook 3.1/starter/workbook31/src/test/java/com/ltp/workbook31/Workbook31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.workbook31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workbook31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
